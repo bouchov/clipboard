0cbd85e2b5eaf9252c3352ecd1007369dad63e1b
bouchov <bouchov@intrice.ru>
2021-07-21 18:35:09 +0700

    tests configuration

--- src/test/java/com/bouchov/clipboard/ClipboardApplicationTests.java
+++ src/test/java/com/bouchov/clipboard/ClipboardApplicationTests.java
@@ -2,8 +2,10 @@ package com.bouchov.clipboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class ClipboardApplicationTests {
 
     @Test
